633175c9bbeed851ae0853f4bd3103a35a0eaede
Muyeed Ahmed <akib100095@gmail.com>
2026-01-19 03:43:39 -0500

    Minor bug

--- Main.py
+++ Main.py
@@ -48,8 +48,8 @@ if __name__ == "__main__":
         sys.exit(1)
     dataset_name = sys.argv[1]
     method = sys.argv[2]
-    misclassification_count = int(sys.argv[4]) if len(sys.argv) > 4 else 1
-    cmc_type = sys.argv[5] if len(sys.argv) > 5 else "Any"
+    misclassification_count = int(sys.argv[3]) if len(sys.argv) > 3 else 1
+    cmc_type = sys.argv[4] if len(sys.argv) > 4 else "Any"
 
     os.makedirs(f"Stats/{method}", exist_ok=True)    
             
